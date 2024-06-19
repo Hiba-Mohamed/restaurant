7c8fd0f8243f131ac889d39e4cd5e70e1f08a6cd
Hiba-Mohamed <hibamoha987@gmail.com>
2024-06-19 20:29:35 -0230

    order details list created

---
+++ rest.js
@@ -0,0 +1,71 @@
+        const menuPrices = 
+       [
+            ["hummus", 6.99],
+            ["ghanoush", 7.49],
+            ["falafel",5.99],
+            ["dolma",6.49],
+            ["labneh",5.99],
+            ["shish",16.99],
+            ["shawarma",14.99],
+            ["adana",15.99],
+            ["kofta",15.49],
+            ["grill",22.99],
+            ["moussaka",13.99],
+            ["falafelPlate",11.99],
+            ["peppers",12.99],
+            ["rice",3.99],
+            ["bulgur",4.49],
+            ["tabbouleh",4.99],
+            ["vegGrill",4.99],
+            ["baklava",4.99],
+            ["kunafa",5.99],
+            ["delight",3.99],
+            ["pudding",4.49],
+            ["coffee",2.99],
+            ["tea",2.49],
+            ["ayran",2.99],
+            ["juice",3.99],
+            ["softDrink",1.99]
+          ]
+        
+
+        const submitButton = document.querySelector("#orderSubmit")
+        submitButton.addEventListener('click', function(e){
+            e.preventDefault();
+            let orderDetails=[];
+            let subtotal = 0;
+            const priceListLength = menuPrices.length;
+            console.log(priceListLength)
+            const inputFieldsLength = document.querySelector("#orderForm").elements.length;
+            console.log(inputFieldsLength)
+            for (var i=0; i < inputFieldsLength; i++)
+                {
+                    if(document.querySelector("#orderForm").elements[i].value !== "" 
+                    && document.querySelector("#orderForm").elements[i].value !== 0)
+                    {
+                        const itemName = document.querySelector("#orderForm").elements[i].name;
+                        const itemQuant = document.querySelector("#orderForm").elements[i].value;
+                        parseInt(itemQuant)
+                        console.log(itemName)
+                        console.log(itemQuant)
+                    
+                        for (var j = 0; j < priceListLength; j++) {
+                        if (menuPrices[j][0] === itemName)
+                            {
+                            const itemPrice =  menuPrices[j][1];
+                            console.log(itemPrice)
+                            const totalPrice = itemPrice * itemQuant
+                            subtotal += totalPrice
+                            orderDetails.push({itemName,itemQuant,itemPrice,totalPrice})
+                            console.log(orderDetails)
+                            }
+                    }
+                    console.log(subtotal)
+
+                    }
+                }
+
+            
+        })
+    
+
