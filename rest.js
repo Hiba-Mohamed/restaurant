a39fa2931c4dac2eedb5fac08072658c26c58668
Hiba-Mohamed <hibamoha987@gmail.com>
2024-06-20 20:23:25 -0230

    receipt structure

--- rest.js
+++ rest.js
@@ -12,36 +12,36 @@ else{
 }
 })       
         
-        const menuPrices = 
-       [
-            ["hummus", 6.99],
-            ["ghanoush", 7.49],
-            ["falafel",5.99],
-            ["dolma",6.49],
-            ["labneh",5.99],
-            ["shish",16.99],
-            ["shawarma",14.99],
-            ["adana",15.99],
-            ["kofta",15.49],
-            ["grill",22.99],
-            ["moussaka",13.99],
-            ["falafelPlate",11.99],
-            ["peppers",12.99],
-            ["rice",3.99],
-            ["bulgur",4.49],
-            ["tabbouleh",4.99],
-            ["vegGrill",4.99],
-            ["baklava",4.99],
-            ["kunafa",5.99],
-            ["delight",3.99],
-            ["pudding",4.49],
-            ["coffee",2.99],
-            ["tea",2.49],
-            ["ayran",2.99],
-            ["juice",3.99],
-            ["softDrink",1.99]
-          ]
-        
+const menuPrices = 
+    [
+        ["hummus", 6.99],
+        ["ghanoush", 7.49],
+        ["falafel",5.99],
+        ["dolma",6.49],
+        ["labneh",5.99],
+        ["shish",16.99],
+        ["shawarma",14.99],
+        ["adana",15.99],
+        ["kofta",15.49],
+        ["grill",22.99],
+        ["moussaka",13.99],
+        ["falafelPlate",11.99],
+        ["peppers",12.99],
+        ["rice",3.99],
+        ["bulgur",4.49],
+        ["tabbouleh",4.99],
+        ["vegGrill",4.99],
+        ["baklava",4.99],
+        ["kunafa",5.99],
+        ["delight",3.99],
+        ["pudding",4.49],
+        ["coffee",2.99],
+        ["tea",2.49],
+        ["ayran",2.99],
+        ["juice",3.99],
+        ["softDrink",1.99]
+    ]
+            
 
         const submitButton = document.querySelector("#orderSubmit")
         submitButton.addEventListener('click', function(e){
@@ -80,7 +80,7 @@ else{
                         console.log(priceListLength)
                         const inputFieldsLength = document.querySelector("#orderForm").elements.length;
                         console.log(inputFieldsLength)
-                        for (var i=0; i < inputFieldsLength; i++)
+                        for (var i=0; i < inputFieldsLength-3; i++)
                             {
                                 if(document.querySelector("#orderForm").elements[i].value !== "" 
                                 && document.querySelector("#orderForm").elements[i].value !== 0)
@@ -118,19 +118,59 @@ else{
                             }
             
                             const OrderDetailsTable = document.querySelector('#orderDetailsTable')
+                            OrderDetailsTable.innerHTML =`<th class="tableHead">
+                                                            <h4>Your Order Details</h4>
+                                                          </th>
+                                                          <div class="custDispDiv">
+                                                          <tr>
+                                                            <td class="custDisInfo">${customerName}</td>
+                                                          </tr>
+                                                          <tr>
+                                                            <td class="custDisInfo">${customerCard}</td>
+                                                          </tr> 
+                                                          <tr>
+                                                            <td class="custDisInfo">${customerAddress} </td>
+                                                          </tr>
+                                                          <tr>
+                                                          <br/>
+                                                          </tr>
+                                                          
+                                                          </div>`
+                                                          
             
                             orderDetails.forEach(element => {
                                 const output = `
                                 <tr>
                                     <td>${element.itemName}</td>
-                                    <td>${element.itemPrice}$</td>
-                                    <td>X</td>
-                                    <td>${element.itemQuant} = </td>
-                                    <td>${element.totalPrice}$</td>
+                                    <td>$${element.itemPrice}</td>
+                                    <td>x${element.itemQuant} </td>
+                                    <td>$${element.totalPrice}</td>
                                 </tr>`
             
                                 OrderDetailsTable.innerHTML += output
                             })
+
+                            OrderDetailsTable.innerHTML += `
+                            <hr/>
+                            <div class ="calculationDiv">
+                                <tr>
+                                    <td>Subtotal</td>
+                                    <td>$${subtotal}</td>
+                                </tr>
+                                <tr>
+                                    <td>Taxes</td>
+                                    <td>$${(subtotal*0.15).toFixed(2)}</td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2">
+                                    <hr/>
+                                    </td>
+                                </tr>
+                                  <tr>
+                                    <td>Total</td>
+                                    <td>$${(subtotal* 1.12).toFixed(2)}</td>
+                                </tr>
+                                </div>`                            
             
                     }
                 }
